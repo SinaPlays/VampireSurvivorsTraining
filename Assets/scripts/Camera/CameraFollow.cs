9ccd8843b4ba2673cebbb8eeebdfed96c510d81a
SinaPlays <30eana@gmail.com>
2025-10-04 14:30:44 +0200

    UpdateCamera

--- Assets/scripts/Camera/CameraFollow.cs
+++ Assets/scripts/Camera/CameraFollow.cs
@@ -5,7 +5,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform player;
     Vector3 playerPosition;
 
-    void Update()
+    public void UpdateCamera()
     {
         playerPosition = player.position;
         transform.position = new Vector3(playerPosition.x, playerPosition.y, -10);
