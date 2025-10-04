24a2a29654c3759b5d5ea433b87698bd6cdb0ff4
SinaPlays <30eana@gmail.com>
2025-10-04 14:15:44 +0200

    GameManager

--- Assets/scripts/Managers/GameManager.cs
+++ Assets/scripts/Managers/GameManager.cs
@@ -1,26 +1,79 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
 
-    void Awake()
+    public static GameManager instance;
+    private void Awake()
     {
-        if (Instance != null && Instance != this)
+        if (instance != null && instance != this)
         {
-            Destroy(gameObject);
+            Debug.LogWarning("Multiple instances of StateMachine detected. Destroying duplicate.");
             return;
         }
-        Instance = this;
+        instance = this;
     }
-
+    public enum GameState
+    {
+        MainMenu,
+        Playing,
+        Paused,
+        GameOver,
+        UpgradeMenu
+    }
+    public GameState currentState;
     void Start()
     {
-
+        currentState = GameState.MainMenu;
     }
 
     void Update()
     {
+        switch (currentState)
+        {
+            case GameState.MainMenu:
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    ChangeState(GameState.Playing);
+                }
+                break;
+            case GameState.Playing:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    ChangeState(GameState.Paused);
+                    Time.timeScale = 0;
+                }
+                break;
+            case GameState.Paused:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    ChangeState(GameState.Playing);
+                    Time.timeScale = 1;
+                }
+                break;
+            case GameState.GameOver:
 
+                break;
+            case GameState.UpgradeMenu:
+                
+                break;
+        }
+    }
+    public void ChangeState(GameState aState)
+    {
+        switch (aState)
+        {
+            case GameState.MainMenu:
+                break;
+            case GameState.Playing:
+                break;
+            case GameState.Paused:
+                break;
+            case GameState.GameOver:
+                break;
+        }
+
+        currentState = aState;
     }
 }
